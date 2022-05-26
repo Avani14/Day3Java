89a3e125f5b7bb454ca2182e3f6c51b979c4de95
Avani14 <trivediavani14@gmail.com>
2022-05-26 16:14:25 +0530

    Attendance checking of employee

--- src/Attendance/CheckAttendance.java
+++ src/Attendance/CheckAttendance.java
@@ -3,5 +3,13 @@ package Attendance;
 public class CheckAttendance {
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee wage computation program.");
+		double empAttendance = Math.floor(Math.random()*10)%2;
+        if(empAttendance == 1)
+        {
+            System.out.println("Employee is present today");
+        }
+        else {
+            System.out.println("Employee is not present today");
+        }
 	}
 }
