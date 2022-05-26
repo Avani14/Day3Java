c592c48841ddda322d80afbd7ca79f886b8adf7c
Avani14 <trivediavani14@gmail.com>
2022-05-26 17:43:34 +0530

    Monthly wage

--- src/Attendance/CheckAttendance.java
+++ src/Attendance/CheckAttendance.java
@@ -4,51 +4,33 @@ public class CheckAttendance {
 	static int wagePerHour = 20;
 	static int fullDayHour = 8;
 	static int partTimeHour = 4;
+	static int workingDays = 20;
 	static int wageCalculation(int x,int y)
 	{
 		int ans = x * y;
 		return ans;
 	}
-	
-	// Solving the problem of daily wage using switch case statement
-	static void wageCalculationUsingSwitch(int workingHours)
+	static void monthlyWage(int x)
 	{
-		switch(workingHours) {
-		case 8:
-			System.out.println("Daily wage of full time employee is : "+wagePerHour*fullDayHour);
-			break;
-		case 4:
-			System.out.println("Daily wage of part time employee is : "+wagePerHour*partTimeHour);
-			break;
-		}
+		int dailyWageFullTime = CheckAttendance.wageCalculation(wagePerHour,fullDayHour);
+		System.out.println("Monthly wage of a full time employee is : "+dailyWageFullTime*x);
+		int dailyWagePartTime = CheckAttendance.wageCalculation(wagePerHour,partTimeHour);
+		System.out.println("Monthly wage of a full time employee is : "+dailyWagePartTime*x);
+		
 	}
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to employee wage computation program.");
-//		double empAttendance = Math.floor(Math.random()*10)%2;
-//        if(empAttendance == 1)
-//        {
-//            System.out.println("Employee is present today");
-//        }
-//        else {
-//            System.out.println("Employee is not present today");
-//        }
-//        System.out.println("Daily wage of an employee is : "+CheckAttendance.wageCalculation(wagePerHour, fullDayHour));
-//        System.out.println("Daily wage of a part time employee is : "+CheckAttendance.wageCalculation(wagePerHour, partTimeHour));
-        
-		
-		//Solving the problem of attendance using switch case
-		int empAttendance = (int) (Math.floor(Math.random()*10)%2);
-		switch (empAttendance) {
-		case 1 :
-			System.out.println("Employee is present...");
-			break;
-		case 0: 
-			System.out.println("Employee is absent...");
-		}
-		CheckAttendance.wageCalculationUsingSwitch(8);
-		CheckAttendance.wageCalculationUsingSwitch(4);
-		
-		
+		double empAttendance = Math.floor(Math.random()*10)%2;
+        if(empAttendance == 1)
+        {
+            System.out.println("Employee is present today");
+        }
+        else {
+            System.out.println("Employee is not present today");
+        }
+        System.out.println("Daily wage of an employee is : "+CheckAttendance.wageCalculation(wagePerHour, fullDayHour));
+        System.out.println("Daily wage of a part time employee is : "+CheckAttendance.wageCalculation(wagePerHour, partTimeHour));
+        monthlyWage(workingDays);
 	}
 }
