a9184a6cd7af858367e97d7298e8df8d6572053c
Avani14 <trivediavani14@gmail.com>
2022-05-26 16:27:44 +0530

    Daily wage calculation

--- src/Attendance/CheckAttendance.java
+++ src/Attendance/CheckAttendance.java
@@ -1,7 +1,17 @@
 package Attendance;
 
 public class CheckAttendance {
+	static int wagePerHour = 20;
+	static int fullDayHour = 8;
+	
+	static int wageCalculation(int x,int y)
+	{
+		int ans = x * y;
+		return ans;
+	}
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to employee wage computation program.");
 		double empAttendance = Math.floor(Math.random()*10)%2;
         if(empAttendance == 1)
@@ -11,5 +21,6 @@ public class CheckAttendance {
         else {
             System.out.println("Employee is not present today");
         }
+        System.out.println("Daily wage of an employee is : "+CheckAttendance.wageCalculation(wagePerHour, fullDayHour));
 	}
 }
