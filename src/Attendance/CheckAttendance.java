2f985d4f56f92299b2b67fcc193e4207e657ad52
Avani14 <60188040+Avani14@users.noreply.github.com>
2022-05-26 15:58:07 +0530

    Update CheckAttendance.java
    Changes according to the assignment

--- src/Attendance/CheckAttendance.java
+++ src/Attendance/CheckAttendance.java
@@ -2,13 +2,6 @@ package Attendance;
 
 public class CheckAttendance {
 	public static void main(String[] args) {
-		double empAttendance = Math.floor(Math.random()*10)%2;
-		if(empAttendance == 1)
-		{
-			System.out.println("Employee is present today");
-		}
-		else {
-			System.out.println("Employee is not present today");
-		}	
+		System.out.println("Welcome to employee wage computation program.")
 	}
 }
